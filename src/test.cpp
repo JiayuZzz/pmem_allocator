ccbd6417dfb155df4391ca0ef17c50cfff52933a
Jiayu Wu <jiayu.wu@intel.com>
2021-11-03 11:17:14 +0800

    fix

--- src/test.cpp
+++ src/test.cpp
@@ -10,26 +10,30 @@ int main() {
     int block_size = 32;
     bool done = false;
     std::atomic<uint64_t> ops;
-//    memkind_t kind;
-//    memkind_create_pmem("/mnt/pmem0/memkind", 100ULL * 1024 * 1024 * 1024, &kind);
+    memkind_t kind;
+    memkind_create_pmem("/mnt/pmem0/memkind", 100ULL * 1024 * 1024 * 1024, &kind);
     PMEMAllocator *allocator = PMEMAllocator::NewPMEMAllocator("/mnt/pmem0/pool", 1ULL * 100 * 1024 * 1024 * 1024,
                                                                1024 * 1024 / block_size,
                                                                block_size, threads, false);
     std::vector<std::thread> ths;
     uint64_t cycle = 1024 * 1024;
     int cnt = 1024;
-    std::vector<PMemSpaceEntry> entries(cnt);
-    std::vector<void *> pointers(cnt);
     auto bench = [&](int tid) {
+        std::vector<PMemSpaceEntry> entries(cnt);
+        std::vector<void *> pointers(cnt);
         for (int i = 1; i <= cycle; i++) {
             if (done) return;
             int allocate_size = (cycle * block_size) % 1025;
 
             for (int j = 0; j < cnt; j++) {
+//                pointers[j] = malloc(allocate_size);
+//                pointers[j] = memkind_malloc(kind, allocate_size);
                 entries[j] = allocator->Allocate(allocate_size);
             }
 
             for (int j = 0; j < cnt; j++) {
+//                free(pointers[j]);
+//                memkind_free(kind, pointers[j]);
                 allocator->Free(entries[j]);
             }
             ops += cnt;
